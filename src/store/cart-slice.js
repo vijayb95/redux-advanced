fb7130fcfe5e4cdaa2efaebb4e36fadc888617e1
Vijayabalaji B <jayb2495@gmail.com>
2023-06-12 20:05:05 +0530

    final

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -6,12 +6,18 @@ const cartSlice = createSlice({
     items: [],
     totalQuantity: 0,
     totalAmount: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
